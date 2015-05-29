663c32b078e561f00ffe2decc6c030061d6825d0
Graham Sutton <grahamsutton2@gmail.com>
2015-05-29 19:44:29 -0400

    added slug column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150529231615) do
+ActiveRecord::Schema.define(version: 20150529234407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20150529231615) do
     t.integer  "role"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "slug"
   end
 
   add_foreign_key "invoices", "listings"
