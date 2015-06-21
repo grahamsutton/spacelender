7e00203e496d0722b5586f108b798e260467bd7c
Graham Sutton <grahamsutton2@gmail.com>
2015-06-21 19:04:48 -0400

    made geocoding a little more ambiguous for user privacy concerns. However, the form seems to not be submitting the State name during form submissions. Needs fixing.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,10 +1,14 @@
 class Location < ActiveRecord::Base
   belongs_to :listing
 
-  geocoded_by :full_street_address
+  geocoded_by :ambiguous_address
   after_validation :geocode
 
   def full_street_address
   	"#{self.street_address}, #{self.city}, #{self.state} #{self.zip}"
+  end
+
+  def ambiguous_address
+  	"#{self.city}, #{self.state} #{self.zip} "
   end
 end
