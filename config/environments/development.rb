b832be3748cffc66da1b19faf7ed6c6171f7e7d3
Graham Sutton <grahamsutton2@gmail.com>
2015-10-25 22:33:19 -0400

    adjust application font settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,6 +19,8 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  config.assets.paths << Rails.root.join('/app/assets/fonts')
+
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
